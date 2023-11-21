45ac375d3c7990fa38b5aa09b54ef748147f9364
apprehen <3514392356@qq.com>
2023-11-21 18:07:31 +0800

    Site updated: 2023-11-21 18:07:31

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2023/11/20/Linux代理/"];function toRandomPost(){
+var posts=["2023/11/20/python爬虫图片/","2023/11/20/Linux代理/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
