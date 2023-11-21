20c7f7ad04ac16f6a1b0aec291733cf3778a5bae
apprehen <3514392356@qq.com>
2023-11-21 21:47:04 +0800

    Site updated: 2023-11-21 21:47:04

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2023/11/20/Linux代理/","2023/11/20/python爬虫图片/"];function toRandomPost(){
+var posts=["2023/11/20/Linux代理/","2023/11/21/Nginx基本使用/","2023/11/20/python爬虫图片/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
