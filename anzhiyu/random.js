bb1ce902ecbcb8be2ce2032a64d808aacb2a4a2b
apprehen <3514392356@qq.com>
2023-11-25 10:58:08 +0000

    update the post Updated By Github Actions

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2023/11/21/Javascript详解/","2023/11/20/Linux代理/","2023/11/21/Nginx基本使用/","2023/11/25/git使用/","2023/11/20/python爬虫图片/"];function toRandomPost(){
+var posts=["2023/11/21/Javascript详解/","2023/11/20/Linux代理/","2023/11/21/Nginx基本使用/","2023/11/25/Vue源码分析/","2023/11/25/git使用/","2023/11/20/python爬虫图片/","2023/11/25/打包工具/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
